ad80c2519240d198ac9b0abfa0da43020745af71
Mateusz Dzieżok <oosiekk@gmail.com>
2024-04-29 20:03:42 +0200

    Trying to do the same thing but for another website

--- main.py
+++ main.py
@@ -30,74 +30,7 @@ while True:
 
     ogloszenia = driver.find_elements(By.CLASS_NAME, 'css-1sw7q4x')
 
-    links = []
-    for ogloszenie in ogloszenia:
-        a_elements = ogloszenie.find_elements(By.CSS_SELECTOR, 'a')
-        if a_elements:
-            links.append(a_elements[0].get_attribute('href'))
-
-    for link in links:
-        print(iterator)
-        iterator += 1
-        
-        #for fast debbuging
-        #if(iterator > 3): break
-        
-        row = []
-        row.append(iterator)
-        row.append('=HYPERLINK("'+link+'")')
-        driver.get(link)
-        if 'www.otodom.pl' in link:
-            print("otodom")
-            driver.back()
-            continue
-        
-        offer = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, 'css-1wws9er'))
-        )
-        
-        title = offer[0].find_elements(By.CLASS_NAME, 'css-1yzzyg0')
-        row.append(title[1].find_element(By.CLASS_NAME, 'css-77x51t').text)
-        
-        row.append(offer[0].find_element(By.CLASS_NAME, 'css-sxnu2o').find_element(By.CLASS_NAME, 'css-e2ir3r').find_element(By.TAG_NAME, 'h3').text)
-        
-        dane_container = offer[0].find_element(By.TAG_NAME, 'ul')
-        dane = dane_container.find_elements(By.CLASS_NAME, 'css-1r0si1e')
-        for dana in dane:
-            if 'Czynsz (dodatkowo):' in dana.text:
-                row.append(dana.text.split("Czynsz (dodatkowo):",1)[1])
-        
-        row.append(offer[0].find_element(By.CLASS_NAME, 'css-1m8mzwg').text)
-        
-        driver.back()
-        
-        data.append(row)
-        
-        for total_count in driver.find_elements(By.CSS_SELECTOR, "span[data-testid='total-count']"): 
-            znaleziono_string = total_count.text
-            print(znaleziono_string)
-            lista_znaleziono = znaleziono_string.split()
-            #lista_znaleziono[1]="3"
-            print( int(lista_znaleziono[1]))
-            if int(lista_znaleziono[1])<=iterator:
-                break
-        if int(lista_znaleziono[1])<=iterator:
-            break
-    if int(lista_znaleziono[1])<=iterator:
-        break
-    try:
-        next_buttons = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a[data-testid='pagination-forward']"))
-        )
-        for next_button in next_buttons:
-            print(next_button.get_attribute('href'))
-        
-        driver.get(next_buttons[0].get_attribute('href'))
-        time.sleep(5)
-        #next_button.click()
-    except Exception:
-        logger.error('Failed to do something: ' + str(Exception))
-        break
+    break
 
 driver.quit()
 
